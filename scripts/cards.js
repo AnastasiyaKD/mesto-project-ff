ff9dd829b9e2abe3f11c77255329caa3b9176120
Anastasiya K <AnastasiyaFront01@gmail.com>
2024-01-23 12:04:00 +0300

    bbb

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,25 +2,31 @@ const initialCards = [
     {
       name: "Архыз",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      alt: "Горы с растительностью",
     },
     {
       name: "Челябинская область",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      alt: "Горы в снегу с рекой",
     },
     {
       name: "Иваново",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      alt: "много панельных домов",
     },
     {
       name: "Камчатка",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      alt: "Гора на заднем плане, на первом плане предгорная раснина с растительностью",
     },
     {
       name: "Холмогорский район",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      alt: "Рельсы пролегающие через лес",
     },
     {
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      alt: "Гора на берегу заледеневшего озера",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,27 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const template = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
+
+initialCards.forEach(function (element) {
+  const card = createCard(element);
+  placesList.prepend(card);
+});
+
+function createCard(element) {
+  const card = template.querySelector('.places__item').cloneNode(true);
+  card.querySelector('.card__title').textContent = element.name;
+  card.querySelector('.card__image').src = element.link;
+  card.querySelector('.card__image').alt = element.alt;
+  const buttonRemove = card.querySelector('.card__delete-button');
+  buttonRemove.addEventListener('click', function () {
+    deliteCard(card);
+  });
+  return card;
+}
+
+function deliteCard(card) {
+  card.remove();
+}
