736bb30c2fd7fd8333bfb84e0325fcd833bf3231
Anastasiya K <AnastasiyaFront01@gmail.com>
2024-01-25 12:33:19 +0300

    bbb

--- scripts/index.js
+++ scripts/index.js
@@ -12,22 +12,22 @@ const template = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
 
 initialCards.forEach(function (element) {
-  const card = createCard(element);
+  const card = createCard(element, deleteCard);
   placesList.prepend(card);
 });
 
-function createCard(element) {
+function createCard(element, deleteCard) {
   const card = template.querySelector('.places__item').cloneNode(true);
   card.querySelector('.card__title').textContent = element.name;
   card.querySelector('.card__image').src = element.link;
   card.querySelector('.card__image').alt = element.alt;
   const buttonRemove = card.querySelector('.card__delete-button');
   buttonRemove.addEventListener('click', function () {
-    deliteCard(card);
+    deleteCard(card);
   });
   return card;
 }
 
-function deliteCard(card) {
+function deleteCard(card) {
   card.remove();
 }
